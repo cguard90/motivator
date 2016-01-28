5c644da38bd5649fb2f176e1a01349df502675b2
Chris Guard <cguard90@gmail.com>
2016-01-28 16:24:30 -0500

    create rails environment, include httparty gem, pry gem, and awesome_print

---
+++ final_project/config/environment.rb
@@ -0,0 +1,19 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+require 'JSON'
+require 'httparty'
+require 'awesome_print'
+def get_some_data(action, param_name, param_value)
+  token = ENV['GIVE_API_TOKEN']
+  puts token
+  url = "https://app.place2give.com/Service.svc/give-api?token=#{token}&format=json&action=#{action}&PageNumber=1&NumPerPage=100&#{param_name}=#{param_value}"
+  response = HTTParty.get(url)
+  return response.parsed_response
+end
+
+  hash = JSON.parse(get_some_data("searchCharities", "keyword", "animals"))
+  ap hash
