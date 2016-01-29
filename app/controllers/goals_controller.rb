085827c116c1fe3ed33818cb5725067b4c6d9514
Luis Fernando Plaz <luisplaz@hotmail.com>
2016-01-29 15:36:38 -0500

    Add routes to gaol controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,40 +1,46 @@
 class GoalsController < ApplicationController
 
-def index
-  @goals =Goal.all
-end
-
-def show
-  @goal = Goal.find_by(id: params[:id])
-end
-
-def create
-  @goal = Goal.new(goal_params)
-  @goal.user = current_user
-  if @goal.save
-    redirect goal_path ##TAKE ME TO PROFILE PAGE
-  else
-    render :new
+  def index
+    @goals =Goal.all
   end
-end
 
-def edit
-  @goal.find_by(id: params[:id])
-end
+  def show
+    @goal = Goal.includes(:milestones)find_by(id: params[:id])
+  end
 
-def update
+  def create
+    @goal = Goal.new(goal_params)
+    @goal.user = current_user
+    if @goal.save
+     redirect goal_path ##TAKE ME TO PROFILE PAGE
+    else
+      render :new
+    end
+  end
 
-end
+  def edit
+    @goal.find_by(id: params[:id])
+  end
 
+  def update
+    @goal = Goal.find_by(id: params[:id])
+    if @goal.update_attributes(goal_params)
+      redirect_to goal_path
+    else
+      render :edit
+    end
+  end
 
-def destroy
-
-end
+  def destroy
+    @goal = Goal.find_by(id: params[:id])
+    @goal.destroy
+    redirect_to goal_path
+  end
 
 private
 
-def goal_params
-  params.require(:goal).permit(:title, :description, :limit, :charity_id) ##ASK STEVEN ABOUT USER_ID
-end
+  def goal_params
+    params.require(:goal).permit(:title, :description, :limit, :charity_id) ##ASK STEVEN ABOUT USER_ID
+  end
 
 end
