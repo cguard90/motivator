6583938ce960a860470eb4d241d774d1664a9bec
Chris Guard <cguard90@gmail.com>
2016-01-28 20:50:57 -0500

    fix activerecord association

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,8 @@
 class Goal < ActiveRecord::Base
-  has_one :setter, class_name: "User"
-  has_one :tender, class_name: "User"
-  has_one :charity
+  belongs_to :setter, class_name: "User"
+  belongs_to :tender, class_name: "User"
+  belongs_to :charity
+  has_many :milestones
 
   validates :title, presence: true
   validates :description, presence: true
