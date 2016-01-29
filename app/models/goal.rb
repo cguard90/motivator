b56d8a129793da89676f15bdd34443c973c2fb88
Chris Guard <cguard90@gmail.com>
2016-01-29 11:21:34 -0500

    add message association to goals model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,6 +3,7 @@ class Goal < ActiveRecord::Base
   belongs_to :tender, class_name: "User"
   belongs_to :charity
   has_many :milestones
+  has_many :messages
 
   validates :title, presence: true
   validates :description, presence: true
