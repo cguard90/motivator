a4045b1516373d7634e3cdec33f4d710db8ae578
Luis Fernando Plaz <luisplaz@hotmail.com>
2016-01-29 12:06:39 -0500

    Fix Seed File For Creation Of Charities Through API

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 def charity_data(action, state_name, keyword)
   token = ENV['GIVE_API_TOKEN']
   url = "https://app.place2give.com/Service.svc/give-api?token=#{token}&format=json&action=#{action}&PageNumber=1&NumPerPage=100&ProvState=#{state_name}&Keyword=#{keyword}"
@@ -18,7 +17,7 @@ hash = JSON.parse(charity_data("searchCharities", "NY", "animals"))
 charities = hash['give-api']['data']['charities']['charity']
 
 charities.each do |charity|
-  Charity.create!(name:charity['Name'],type:charity['Type'])
+  Charity.create!(name:charity['Name'],category:charity['Type'])
 end
 
 User.create(username: "something", email: "test@email.com", password: '1234')
