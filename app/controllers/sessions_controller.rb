7b1b8f3d32211fad5e70a3edcab10cba434819c4
Jeff George <webdevjeffus@gmail.com>
2016-01-29 15:52:14 -0500

    Add #oauth_create method to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,17 @@ class SessionsController < ApplicationController
     end
   end
 
+  def oauth_create
+    user = User.create_or_get_from_oauth(request.env['omniauth.auth'])
+    if user
+      flash[:notice] = 'Login successful'
+      redirect_to root_path
+    else
+      flash[:notice] = "Invalid Facebook Login"
+      redirect_to new_session_path
+    end
+  end
+
   def destroy
     session.clear
     redirect_to root_path
