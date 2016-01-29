be03178ea32a024056d0e96b452431af3f535ce7
Luis Fernando Plaz <luisplaz@hotmail.com>
2016-01-28 19:39:32 -0500

    Add Pledge Migration

--- db/migrate/20160129001610_create_charities.rb
+++ db/migrate/20160129001610_create_charities.rb
@@ -1,10 +1,10 @@
 class CreateCharities < ActiveRecord::Migration
   def change
     create_table :charities do |t|
-      t.string    :name,    null: false
-      t.string    :type
+      t.string   :name, null: false
+      t.string   :type
 
-      t.timestamps          null: false
+      t.timestamps      null: false
     end
   end
 end
