5c6d739863bdde7646da168e0f3b5c6f645bcb46
Jeff George <webdevjeffus@gmail.com>
2016-01-30 16:17:05 -0500

    Add delegations to Message and Goal associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,13 @@ class Message < ActiveRecord::Base
   belongs_to :user
 
   validates :content, presence: true
+  # delegate :setter, to: :goal, prefix: true
+  delegate :setter_username, to: :goal, prefix: true
+  delegate :title, to: :goal, prefix: true
+  delegate :username, to: :user
 
   def self.broadcast
-    Message.order(created_at: :desc).limit(20)
+    Message.includes(:user, goal: :setter).order(created_at: :desc).limit(20)
   end
 
 end
