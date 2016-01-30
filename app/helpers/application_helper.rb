ef2d8959bf51cf9188fadef36380f10f3bef8248
Luis Fernando Plaz <luisplaz@hotmail.com>
2016-01-30 18:39:52 -0500

    Add date formatting method in helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     date.strftime(" at %l:%M %P on %b %e, %Y")
   end
 
+  def std_format_day date
+    return "" unless date
+    date.strftime("on %b %e, %Y")
+  end
+
 end
