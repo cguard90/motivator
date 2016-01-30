0a6e064e3086b66f8c672313c6fb1641e954a1ef
Chris Guard <cguard90@gmail.com>
2016-01-30 11:18:31 -0500

    change redirect upon login to go to User Show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to user_path(id: current_user.id)
     else
       flash[:notice] = "Invalid email or password"
       redirect_to new_session_path
