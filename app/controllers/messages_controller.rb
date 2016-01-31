009b6c995c4637cbab2ed735647148c8b9f10e9e
Jeff George <webdevjeffus@gmail.com>
2016-01-30 20:44:49 -0500

    Implement goals/messages/create route

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,4 +4,16 @@ class MessagesController < ApplicationController
     @messages = Message.broadcast
   end
 
+  def create
+    if params[:goal_id]
+      goal = Goal.find_by(id: params[:goal_id])
+    else
+      goal = Pledge.find_by(id: params[:pledge_id]).goal
+    end
+    @message = goal.messages.new(user: current_user, content: params[:message][:content])
+    @message.save
+    @messages = goal.messages
+    redirect_to goal_path(id: goal.id)
+  end
+
 end
