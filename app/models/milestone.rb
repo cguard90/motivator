c5d0c6888ef96860d559fb1b56f0ab41de52c738
Jeff George <webdevjeffus@gmail.com>
2016-02-01 11:29:55 -0500

    Implement setter completion and tender confirmation of milestones

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -4,12 +4,27 @@ class Milestone < ActiveRecord::Base
   validates :description, presence: true
   validates :deadline, presence: true
 
-  def announcement(current_user)
-    Message.create(
-      user_id: 1,
-      goal_id: self.goal_id,
-      content: "#{current_user.username} has confirmed that #{self.goal.setter.username} has completed the milestone '#{self.description}'."
-    )
+  def update_status(user)
+    if user == self.goal.setter
+      self.completed = true
+    else
+      self.confirmed = true
+    end
   end
 
+  def announcement
+    if self.confirmed == true
+      Message.create(
+        user_id: 1,
+        goal_id: self.goal_id,
+        content: "#{self.goal.tender.username} has confirmed that #{self.goal.setter.username} has completed the milestone '#{self.description}'."
+      )
+    else
+      Message.create(
+        user_id: 1,
+        goal_id: self.goal_id,
+        content: "#{self.goal.setter.username} has reported to #{self.goal.tender.username} that the milestone '#{self.description}' is complete."
+      )
+    end
+  end
 end
