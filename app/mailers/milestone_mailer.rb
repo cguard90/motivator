b79dc5c1ffd1575bb7011478e437306d769028ed
Jeff George <webdevjeffus@gmail.com>
2016-02-03 12:23:30 -0500

    Create milestone_mailer.rb

---
+++ app/mailers/milestone_mailer.rb
@@ -0,0 +1,12 @@
+class MilestoneMailer < ApplicationMailer
+
+  def announce_milestone_email(milestone)
+    # @milestone = milestone
+    # @goal = milestone.goal
+    # @setter = milestone.goal.setter
+    # @tender = milestone.goal.tender
+    # if @milestone.complete
+
+  end
+
+end
